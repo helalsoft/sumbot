ba21cb36e433c8c4649b7da10a184d6313bcfe96
helalsoft <helalsoft@proton.me>
2025-12-30 20:03:59 +0300

    test: Remove YouTube test

--- tests/youtube.test.ts
+++
@@ -1,8 +0,0 @@
-import { beforeAll, expect, test } from "vitest";
-import "dotenv/config";
-
-beforeAll(async () => {});
-
-test("should extract captions for a given video ID", async () => {}); // Timeout for this test
-
-test("should throw an error for a video ID without captions", async () => {});
